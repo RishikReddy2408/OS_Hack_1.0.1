eee2034628d31de56a972b2631a02409ae74916b
RishikReddy2408 <49032465+RishikReddy2408@users.noreply.github.com>
2019-10-20 01:01:56 +0530

    Update CallSuper.java

--- android/support/annotation/CallSuper.java
+++ android/support/annotation/CallSuper.java
@@ -1,11 +1,9 @@
-package android.support.annotation;
-
-import java.lang.annotation.Annotation;
-import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-
+package android.support.annotation;
+import java.lang.annotation.Annotation;
+import java.lang.annotation.Documented;
 @Documented
 @Retention(RetentionPolicy.CLASS)
 @Target({java.lang.annotation.ElementType.METHOD})
