b8bb1200ef29effa08a0543b26aba004be92989b
RishikReddy2408 <49032465+RishikReddy2408@users.noreply.github.com>
2019-10-20 01:07:05 +0530

    Update Dimension.java

--- android/support/annotation/Dimension.java
+++ android/support/annotation/Dimension.java
@@ -1,11 +1,9 @@
-package android.support.annotation;
-
-import java.lang.annotation.Annotation;
-import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-
+package android.support.annotation;
+import java.lang.annotation.Annotation;
+import java.lang.annotation.Documented;
 @Documented
 @Retention(RetentionPolicy.CLASS)
 @Target({java.lang.annotation.ElementType.METHOD, java.lang.annotation.ElementType.PARAMETER, java.lang.annotation.ElementType.FIELD, java.lang.annotation.ElementType.LOCAL_VARIABLE, java.lang.annotation.ElementType.ANNOTATION_TYPE})
@@ -14,6 +12,5 @@ public @interface Dimension
   public static final int A = 2;
   public static final int L = 0;
   public static final int TRUE = 1;
-  
   int unit();
 }
