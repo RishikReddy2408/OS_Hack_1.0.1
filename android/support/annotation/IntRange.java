2d731d014ebf4bf3da6cea51862b9af9ae84d1bf
RishikReddy2408 <49032465+RishikReddy2408@users.noreply.github.com>
2019-10-20 01:14:43 +0530

    Update IntRange.java

--- android/support/annotation/IntRange.java
+++ android/support/annotation/IntRange.java
@@ -1,10 +1,8 @@
 package android.support.annotation;
-
-import java.lang.annotation.Annotation;
-import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-
+import java.lang.annotation.Annotation;
+import java.lang.annotation.Retention;
 @Retention(RetentionPolicy.CLASS)
 @Target({java.lang.annotation.ElementType.METHOD, java.lang.annotation.ElementType.PARAMETER, java.lang.annotation.ElementType.FIELD, java.lang.annotation.ElementType.LOCAL_VARIABLE, java.lang.annotation.ElementType.ANNOTATION_TYPE})
 public @interface IntRange
