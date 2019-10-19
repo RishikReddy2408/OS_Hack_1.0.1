e023fba22efabd51cc6a392450ebcf838fb2b446
RishikReddy2408 <49032465+RishikReddy2408@users.noreply.github.com>
2019-10-20 00:17:04 +0530

    Update TaskExecutor.java

--- android/arch/core/executor/TaskExecutor.java
+++ android/arch/core/executor/TaskExecutor.java
@@ -1,14 +1,15 @@
+//java code for executing tasks
 package android.arch.core.executor;
-
 import android.support.annotation.RestrictTo;
-
+//Restriction
 @RestrictTo({android.support.annotation.RestrictTo.Scope.LIBRARY_GROUP})
 public abstract class TaskExecutor
 {
-  public TaskExecutor() {}
-  
+  public TaskExecutor() //Task Executer
+  {
+    
+  }
   public abstract void executeOnDiskIO(Runnable paramRunnable);
-  
   public void executeOnMainThread(Runnable paramRunnable)
   {
     if (isMainThread())
@@ -18,8 +19,6 @@ public abstract class TaskExecutor
     }
     postToMainThread(paramRunnable);
   }
-  
   public abstract boolean isMainThread();
-  
   public abstract void postToMainThread(Runnable paramRunnable);
 }
