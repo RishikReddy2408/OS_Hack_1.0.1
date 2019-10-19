1c6bd5911725979df5806ecab2edb03486bc59c7
RishikReddy2408 <49032465+RishikReddy2408@users.noreply.github.com>
2019-10-20 01:14:00 +0530

    Update IntDef.java

--- android/support/annotation/IntDef.java
+++ android/support/annotation/IntDef.java
@@ -1,10 +1,8 @@
 package android.support.annotation;
-
-import java.lang.annotation.Annotation;
-import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-
+import java.lang.annotation.Annotation;
+import java.lang.annotation.Retention;
 @Retention(RetentionPolicy.SOURCE)
 @Target({java.lang.annotation.ElementType.ANNOTATION_TYPE})
 public @interface IntDef
