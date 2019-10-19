b229e9f5bb5c7a3bdc41513b29798cf5117df905
RishikReddy2408 <49032465+RishikReddy2408@users.noreply.github.com>
2019-10-20 00:32:46 +0530

    Update FastSafeIterableMap.java

--- android/arch/core/internal/FastSafeIterableMap.java
+++ android/arch/core/internal/FastSafeIterableMap.java
@@ -1,17 +1,14 @@
 package android.arch.core.internal;
-
-import android.support.annotation.RestrictTo;
 import java.util.HashMap;
 import java.util.Map.Entry;
-
+import android.support.annotation.RestrictTo;
+//Restriction
 @RestrictTo({android.support.annotation.RestrictTo.Scope.LIBRARY_GROUP})
 public class FastSafeIterableMap<K, V>
   extends SafeIterableMap<K, V>
 {
-  private HashMap<K, SafeIterableMap.Entry<K, V>> mHashMap = new HashMap();
-  
+  private HashMap<K, SafeIterableMap.Entry<K, V>> mHashMap = new HashMap();//Using HashMaps
   public FastSafeIterableMap() {}
-  
   public Map.Entry ceil(Object paramObject)
   {
     if (contains(paramObject)) {
@@ -19,18 +16,15 @@ public class FastSafeIterableMap<K, V>
     }
     return null;
   }
-  
-  public boolean contains(Object paramObject)
+  public boolean contains(Object paramObject)  //Checking the 
   {
     return mHashMap.containsKey(paramObject);
   }
-  
-  protected SafeIterableMap.Entry get(Object paramObject)
+  protected SafeIterableMap.Entry get(Object paramObject)  //Entry
   {
     return (SafeIterableMap.Entry)mHashMap.get(paramObject);
   }
-  
-  public Object putIfAbsent(Object paramObject1, Object paramObject2)
+  public Object putIfAbsent(Object paramObject1, Object paramObject2)  //Taking the parameters
   {
     SafeIterableMap.Entry localEntry = get(paramObject1);
     if (localEntry != null) {
@@ -39,8 +33,7 @@ public class FastSafeIterableMap<K, V>
     mHashMap.put(paramObject1, append(paramObject1, paramObject2));
     return null;
   }
-  
-  public Object remove(Object paramObject)
+  public Object remove(Object paramObject)  //Removing the parameter paramObject
   {
     Object localObject = super.remove(paramObject);
     mHashMap.remove(paramObject);
