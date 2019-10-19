efabc9e8bf7e5dc569d18f52311fd3e8f887cd55
RishikReddy2408 <49032465+RishikReddy2408@users.noreply.github.com>
2019-10-19 23:05:50 +0530

    Update Security API.c

--- APIs/Security API.c
+++ APIs/Security API.c
@@ -1,58 +1,37 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-#include <sys/types.h>
-#include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
 #include <errno.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-
 #include <gssapi/gssapi.h>
 #include <gssapi/gssapi_ext.h>
 #include <gss-misc.h>
+#include <string.h>
+#include <ctype.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 
-void usage()
+void use()//usage
 {
-     fprintf(stderr, "Usage: gss-client [-port port] [-d] host service \
-msg\n");
+     fprintf(stderr, "Usage: gss-client [-port port] [-d] host service \ msg\n");
      exit(1);
 }
-
-/*
- * Function: connect_to_server
- *
- * Purpose: Opens a TCP connection to the name host and port.
- *
- * Arguments:
- *
- *      host            (r) the target host name
- *      port            (r) the target port, in host byte order
- *
- * Returns: the established socket file descriptor, or -1 on failure
- *
- * Effects:
- *
- * The host name is resolved with gethostbyname(), and the socket is
- * opened and connected.  If an error occurs, an error message is
- * displayed and -1 is returned.
- */
-int connect_to_server(host, port)
+int server_connection(host, port)
      char *host;
      u_short port;
 {
      struct sockaddr_in saddr;
      struct hostent *hp;
      int s;
-     
+     // Resolving the IP of the host
      if ((hp = gethostbyname(host)) == NULL) {
           fprintf(stderr, "Unknown host: %s\n", host);
           return -1;
      }
-     
+     // port number
      saddr.sin_family = hp->h_addrtype;
      memcpy((char *)&saddr.sin_addr, hp->h_addr, sizeof(saddr.sin_addr));
      saddr.sin_port = htons(port);
@@ -61,8 +40,9 @@ int connect_to_server(host, port)
           perror("creating socket");
           return -1;
      }
+     // creating a socket
      if (connect(s, (struct sockaddr *)&saddr, sizeof(saddr)) < 0) {
-          perror("connecting to server");
+          perror("server connecting...");//connecting to server 
           (void) close(s);
           return -1;
      }
