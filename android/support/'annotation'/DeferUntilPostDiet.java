990ca07b91a3b913e324badfde3b915f7cdf6f42
RishikReddy2408 <49032465+RishikReddy2408@users.noreply.github.com>
2019-10-20 00:46:13 +0530

    Update DeferUntilPostDiet.java

--- android/support/'annotation'/DeferUntilPostDiet.java
+++ android/support/'annotation'/DeferUntilPostDiet.java
@@ -1,14 +1,15 @@
 package android.support.'annotation';
-
-import android.support.annotation.Dimension;
-import java.lang.annotation.Annotation;
-import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-
+import android.support.annotation.Dimension;
+import java.lang.annotation.Annotation;
+import java.lang.annotation.Documented;
 @Documented
 @Retention(RetentionPolicy.CLASS)
 @Target({java.lang.annotation.ElementType.METHOD, java.lang.annotation.ElementType.PARAMETER, java.lang.annotation.ElementType.FIELD, java.lang.annotation.ElementType.LOCAL_VARIABLE})
 @Dimension(unit=1)
-public @interface DeferUntilPostDiet {}
+public @interface DeferUntilPostDiet 
+{
+
+}
