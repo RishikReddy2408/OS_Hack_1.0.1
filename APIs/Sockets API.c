f47d3543d6e11d522d5b25b20fd3f7987086521b
RishikReddy2408 <49032465+RishikReddy2408@users.noreply.github.com>
2019-10-19 23:28:31 +0530

    Update Sockets API.c

--- APIs/Sockets API.c
+++ APIs/Sockets API.c
@@ -8,23 +8,22 @@
 #define PORT 8080 
 int main(int argc, char const *argv[]) 
 { 
-	int server_fd, new_socket, valread; 
-	struct sockaddr_in address; 
+	int s_f, n_s, value_read; 
+	struct socketaddr_in address; 
 	int opt = 1; 
-	int addrlen = sizeof(address); 
+	int addresslength = sizeof(address); 
 	char buffer[1024] = {0}; 
 	char *hello = "Hello from server side"; 
 	
-	//  Initializing a socket file descriptor 
-	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
+	//  Here the socket file descriptor would be initialized  
+	if ((s_f = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
 	{ 
-		perror("socket failed"); 
+		perror("socket failed"); // error on network failure
 		exit(EXIT_FAILURE); 
 	} 
 	
-	// Attaching socket forcefully to the port 8080 
-	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, 
-												&opt, sizeof(opt))) 
+	// Forcefully attaching the socket to PORT number 8080
+	if (setsockopt(s_f, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) // use of reuseaddr and reuseport API using pipes 
 	{ 
 		perror("setsockopt"); 
 		exit(EXIT_FAILURE); 
@@ -33,28 +32,26 @@ int main(int argc, char const *argv[])
 	address.sin_addr.s_addr = INADDR_ANY; 
 	address.sin_port = htons( PORT ); 
 	
-	// Forcefully attaching socket to the port 8080 
-	if (bind(server_fd, (struct sockaddr *)&address, 
-								sizeof(address))<0) 
+	// Enabling socket to the port 8080 for all the other network IP 
+	if (bind(s_f, (struct socketaddr *)&address,sizeof(address))<0) 
 	{ 
-		perror("bind failed"); 
+		perror("bindfold failed"); 
 		exit(EXIT_FAILURE); 
 	} 
-	if (listen(server_fd, 3) < 0) 
+	if (listen(s_f, 3) < 0) 
 	{ 
-		perror("listen"); 
+		perror("listening"); 
+		exit(EXIT_FAILURE);// failure to fin the exit  
+	} 
+	if ((n_s = accept(s_f, (struct sockaddr *)&address, (socklen_t*)&addresslength))<0) 
+	{ 
+		perror("accepting"); 
 		exit(EXIT_FAILURE); 
 	} 
-	if ((new_socket = accept(server_fd, (struct sockaddr *)&address, 
-					(socklen_t*)&addrlen))<0) 
-	{ 
-		perror("accept"); 
-		exit(EXIT_FAILURE); 
-	} 
-	valread = read( new_socket , buffer, 1024); 
-	printf("%s\n",buffer ); 
-	send(new_socket , hello , strlen(hello) , 0 ); 
-	printf("Hello message sent\n"); 
+	value_read = read( n_s , buffer, 1024); 
+	printf("%s\n\n",buffer ); 
+	send(n_s , hello , strlen(hello) , 0 ); 
+	printf("Hello message sent\n\n"); 
 	return 0; 
 } 
 
