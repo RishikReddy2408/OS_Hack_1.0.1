07fd0afe710d6f66d8a9844cd44a7d8da396408b
RishikReddy2408 <49032465+RishikReddy2408@users.noreply.github.com>
2019-10-19 23:58:01 +0530

    Update Motion-Detection.js

--- APIs/Motion-Detection.js
+++ APIs/Motion-Detection.js
@@ -1,14 +1,21 @@
 let torusGeometry = new THREE.TorusGeometry(7, 1.6, 4, 3, 6.3);
+/*
+TorusGeometry(radius, tube , radialSegments , tubularSegments , arc )
+radius - Radius of the torus, from the center of the torus to the center of the tube. 1(by default).
+tube — Radius of the tube. 0.4(by default).
+radialSegments — 8(by default)
+tubularSegments — 6(by default)
+arc — Central angle. Default is Math.PI * 2.*/
 let material = new THREE.MeshBasicMaterial({ color: 0x0071C5 });
 let torus = new THREE.Mesh(torusGeometry, material);
 scene.add(torus);
 
-// Update mesh rotation using quaternion.
+// quaternion to update mesh rotation.
 const sensorAbs = new AbsoluteOrientationSensor();
 sensorAbs.onreading = () => torus.quaternion.fromArray(sensorAbs.quaternion);
 sensorAbs.start();
 
-// Update mesh rotation using rotation matrix.
+//rotation matrix update mesh rotation .
 const sensorRel = new RelativeOrientationSensor();
 let rotationMatrix = new Float32Array(16);
 sensor_rel.onreading = () => {
