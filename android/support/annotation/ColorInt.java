aa6e5a1e57b3780390c053b7e34a5f920438a8a3
RishikReddy2408 <49032465+RishikReddy2408@users.noreply.github.com>
2019-10-20 01:03:08 +0530

    Update ColorInt.java

--- android/support/annotation/ColorInt.java
+++ android/support/annotation/ColorInt.java
@@ -1,10 +1,8 @@
 package android.support.annotation;
-
-import java.lang.annotation.Annotation;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-
+import java.lang.annotation.Annotation;
 @Retention(RetentionPolicy.CLASS)
 @Target({java.lang.annotation.ElementType.PARAMETER, java.lang.annotation.ElementType.METHOD, java.lang.annotation.ElementType.LOCAL_VARIABLE, java.lang.annotation.ElementType.FIELD})
 public @interface ColorInt {}
