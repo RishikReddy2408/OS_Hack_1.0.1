4ee3ef9ce82d89474bf456caf0551097446c223d
RishikReddy2408 <49032465+RishikReddy2408@users.noreply.github.com>
2019-10-20 00:56:47 +0530

    Update ArrayRes.java

--- android/support/annotation/ArrayRes.java
+++ android/support/annotation/ArrayRes.java
@@ -1,11 +1,10 @@
-package android.support.annotation;
-
-import java.lang.annotation.Annotation;
-import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-
+import java.lang.annotation.Annotation;
+package android.support.annotation;
+import java.lang.annotation.Documented;
+//rishik
 @Documented
 @Retention(RetentionPolicy.CLASS)
 @Target({java.lang.annotation.ElementType.METHOD, java.lang.annotation.ElementType.PARAMETER, java.lang.annotation.ElementType.FIELD, java.lang.annotation.ElementType.LOCAL_VARIABLE})
